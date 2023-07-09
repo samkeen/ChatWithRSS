0a32487a3b84281ec9fe0c2b9a1c2683b7f37f6e
Sam Keen <sam.sjk@gmail.com>
2023-07-09 11:19:14 -0700

    Adding comments to app.py

--- app.py
+++ app.py
@@ -19,41 +19,63 @@ OPENAI_TEMPERATURE = float(os.getenv("OPENAI_TEMPERATURE", 0.0))
 
 
 def main():
+  # Check that there is an OPENAI API key loaded
   if not env_loaded or not api_key_added:
     st.error("No .env file found and/or OPENAI_API_KEY not added")
     st.markdown("`cp dist.env .env` Then add your OpenAI API key to the .env file")
     return
   
+
+  # Load the vector db store is not loaded, place it in session memory
   if "doc_search" not in st.session_state:
       load_db()
 
-  # show user input
-  user_question = st.text_input("Ask a question about new releases from AWS")
+
+  # Prompt the user for their question
+  user_question = st.text_input("Ask a question of this RSS feed")
+
+
   if user_question:
+
+    # Have Chroma do a similarity search with the uses's supplied query
+    # This returns 'k' number of similar documents.
     docs = st.session_state.doc_search.similarity_search(user_question, k=CHROMA_K_VALUE)
     
+
+    # Instantiate an OpenAI LLM
     llm=ChatOpenAI(
           temperature=OPENAI_TEMPERATURE,
           model=OPENAI_MODEL, 
       ) # type: ignore
 
 
+    # Form the prompt template we will send to the LLM 
     template = "respond as succinctly as possible. {query}?"
-
     prompt = PromptTemplate(
         input_variables=["query"],
         template=template,
     )
 
 
+    # Instantiate a Question/Answer Langchain chain.
+    # The 'stuff' chain type is the simplest method, whereby you simply stuff all the 
+    #   related data into the prompt as context to pass to the language model.
     chain = load_qa_chain(llm, chain_type="stuff")
+
+
+    # Run the chain and collect the response.
+    # Use 'with' context to track token usage
+    #   see: https://python.langchain.com/docs/modules/model_io/models/llms/how_to/token_usage_tracking
     with get_openai_callback() as cb:
       response = chain.run(input_documents=docs, question=prompt.format(query=user_question))
       print(cb)
         
+
+    # Display the result to the user    
     st.write(response)
 
 
+# Load the vector data store into session memory
 def load_db():
   # Create a Chroma vector store
   embeddings = OpenAIEmbeddings() # type: ignore
